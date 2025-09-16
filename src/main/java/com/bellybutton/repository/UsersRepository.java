fe6a066472edb70314ef92f0680b3b6ff583800f
GayathriJaikumaran <gayathrikabi15@gmail.com>
2025-09-16 14:48:07 +0530

    Added user api's

---
+++ src/main/java/com/bellybutton/repository/UsersRepository.java
@@ -0,0 +1,8 @@
+package com.bellybutton.repository;
+
+import com.bellybutton.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    Users findByEmail(String email);
+}
